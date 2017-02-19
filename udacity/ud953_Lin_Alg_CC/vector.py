10cfcdb27aa7a41ff9656e0137e2d8abd7282f4b
Bruno Berzins <b72uno@gmail.com>
2017-02-19 07:03:08 -0800

    Linear algebra crash course update

--- udacity/ud953_Lin_Alg_CC/vector.py
+++ udacity/ud953_Lin_Alg_CC/vector.py
@@ -1,3 +1,5 @@
+import math
+
 def isanumber(a):
     bool_a = True
     try:
@@ -47,16 +49,41 @@ class Vector(object):
             raise ValueError('Both inputs must be vectors')
 
     @classmethod
-    def scale(self, v1, s):
+    def scale(self, v, s):
         try:
-            if not isinstance(v1, self) and isanumber(s):
+            if not isinstance(v, self) and isanumber(s):
                 raise ValueError
-            return Vector([x * s for x in v1.coordinates])
+            return Vector([x * s for x in v.coordinates])
 
         except ValueError:
-            raise ValueError('You must input a vector and a number (float/int)')
+            raise ValueError('Input must be a vector and a number (float/int)')
+
+    @classmethod
+    def magnitude(self, v):
+        try:
+            if not isinstance(v, self):
+                raise ValueError
+            
+            return math.sqrt(sum([x**2 for x in v.coordinates]))
+
+        except ValueError:
+            raise ValueError('Input must be a vector')
+
+    @classmethod
+    def normalize(self, v):
+        try:
+            return self.scale(v, 1/self.magnitude(v))
+
+        except ZeroDivisionError:
+            raise Exception('Cannot normalize the zero vector')
 
 
 print(Vector.add(Vector([8.218, -9.341]), Vector([-1.129, 2.111])))
 print(Vector.subtract(Vector([7.119, 8.215]), Vector([-8.223, 0.878])))
 print(Vector.scale(Vector([1.671, -1.012, -0.318]), 7.41))
+print('--------------')
+print(Vector.magnitude(Vector([-0.221, 7.437])))
+print(Vector.magnitude(Vector([8.813, -1.331, -6.247])))
+print(Vector.normalize(Vector([5.581, -2.136])))
+print(Vector.normalize(Vector([1.996, 3.108, -4.554])))
+print('--------------')
