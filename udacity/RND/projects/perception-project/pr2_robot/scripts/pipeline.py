7922503385ace7eb37ea66b6b07cdbddba26b7ff
Bruno <b72uno@gmail.com>
2018-04-27 15:46:35 -0700

    statistical outlier filter added

--- udacity/RND/projects/perception-project/pr2_robot/scripts/pipeline.py
+++ udacity/RND/projects/perception-project/pr2_robot/scripts/pipeline.py
@@ -3,15 +3,20 @@
 # import PCL module
 import pcl
 
-filename = 'robo_1.pcd'
+filename = 'robo_2.pcd'
 cloud = pcl.load_XYZRGB(filename)
 
-# Filter noise
+# [START Statistical Outlier Filter]
 fil = cloud.make_statistical_outlier_filter()
-fil.set_mean_k(50)
+fil.set_mean_k(5)
 fil.set_std_dev_mul_thresh(1.0)
-pcl.save(fil.filter(), "scene_inliers.pcd")
 
-fil.set_negative(True)
-pcl.save(fil.filter(), "scene_outliers.pcd")
+#pcl.save(fil.filter(), "scene_inliers.pcd")
+#fil.set_negative(True)
+#pcl.save(fil.filter(), "scene_outliers.pcd")
+
+cloud_filtered = fil.filter()
+# [END Statistical Outlier Filter]
+
+
 
