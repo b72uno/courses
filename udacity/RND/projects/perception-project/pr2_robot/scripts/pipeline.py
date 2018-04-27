2eb27a5983500de1f1779af8bf2646844af244b6
Bruno Berzins <b72uno@gmail.com>
2018-04-28 01:56:25 +0300

    Plane segmentation added

--- udacity/RND/projects/perception-project/pr2_robot/scripts/pipeline.py
+++ udacity/RND/projects/perception-project/pr2_robot/scripts/pipeline.py
@@ -6,7 +6,7 @@ import pcl
 filename = 'robo_2.pcd'
 cloud = pcl.load_XYZRGB(filename)
 
-# [START Statistical Outlier Filter]
+# [START Statistical outlier filter]
 fil = cloud.make_statistical_outlier_filter()
 fil.set_mean_k(5)
 fil.set_std_dev_mul_thresh(1.0)
@@ -16,7 +16,30 @@ fil.set_std_dev_mul_thresh(1.0)
 #pcl.save(fil.filter(), "scene_outliers.pcd")
 
 cloud_filtered = fil.filter()
-# [END Statistical Outlier Filter]
+# [END Statistical outlier filter]
+
+# [START Plane fitting]
+vox = cloud_filtered.make_voxel_grid_filter()
+
+# downsample point cloud
+LEAF_SIZE = 0.001
+vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
+cloud_filtered = vox.filter()
+
+pcl.save(cloud_filtered, "voxel_downsampled.pcd")
+
+# passthrough filter
+passthrough = cloud_filtered.make_passthrough_filter()
+filter_axis = 'z'
+passthrough.set_filter_field_name(filter_axis)
+axis_min = 0.6
+axis_max = 1.1
+passthrough.set_filter_limits(axis_min, axis_max)
+
+cloud_filtered = passthrough.filter()
+
+pcl.save(cloud_filtered, "pass_through_filtered.pcd")
+# [END Plane fitting]
 
 
 
