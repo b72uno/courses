380e504a0624990799f300b8290f8f3c6bc7d7ce
Bruno Berzins <b72uno@gmail.com>
2018-04-28 02:11:03 +0300

    added segmentation

--- udacity/RND/projects/perception-project/pr2_robot/scripts/pipeline.py
+++ udacity/RND/projects/perception-project/pr2_robot/scripts/pipeline.py
@@ -11,9 +11,9 @@ fil = cloud.make_statistical_outlier_filter()
 fil.set_mean_k(5)
 fil.set_std_dev_mul_thresh(1.0)
 
-#pcl.save(fil.filter(), "scene_inliers.pcd")
-#fil.set_negative(True)
-#pcl.save(fil.filter(), "scene_outliers.pcd")
+# pcl.save(fil.filter(), "scene_inliers.pcd")
+# fil.set_negative(True)
+# pcl.save(fil.filter(), "scene_outliers.pcd")
 
 cloud_filtered = fil.filter()
 # [END Statistical outlier filter]
@@ -22,11 +22,10 @@ cloud_filtered = fil.filter()
 vox = cloud_filtered.make_voxel_grid_filter()
 
 # downsample point cloud
-LEAF_SIZE = 0.001
+LEAF_SIZE = 0.01
 vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
 cloud_filtered = vox.filter()
-
-pcl.save(cloud_filtered, "voxel_downsampled.pcd")
+# pcl.save(cloud_filtered, "voxel_downsampled.pcd")
 
 # passthrough filter
 passthrough = cloud_filtered.make_passthrough_filter()
@@ -37,9 +36,23 @@ axis_max = 1.1
 passthrough.set_filter_limits(axis_min, axis_max)
 
 cloud_filtered = passthrough.filter()
-
-pcl.save(cloud_filtered, "pass_through_filtered.pcd")
+# pcl.save(cloud_filtered, "pass_through_filtered.pcd")
 # [END Plane fitting]
+
+# [START Segmentation]
+seg = cloud_filtered.make_segmenter()
+seg.set_model_type(pcl.SACMODEL_PLANE)
+seg.set_method_type(pcl.SAC_RANSAC)
+max_distance = 0.01
+seg.set_distance_threshold(max_distance)
+inliers, coefficients = seg.segment()
+
+extracted_inliers = cloud_filtered.extract(inliers, negative=False)
+pcl.save(extracted_inliers, 'extracted_inliers.pcd')
+
+extracted_outliers = cloud_filtered.extract(inliers, negative=True)
+pcl.save(extracted_outliers, 'extracted_outliers.pcd')
+# [END Segmentation]
 
 
 
