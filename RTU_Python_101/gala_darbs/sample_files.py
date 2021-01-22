801fceaf60c16bc751f600ee74a879df9bb4156b
Bruno Berzins <b72uno@gmail.com>
2021-01-22 13:53:31 +0200

    Fucking egotistical bastard. adds expandtab to vimrc

--- RTU_Python_101/gala_darbs/sample_files.py
+++ RTU_Python_101/gala_darbs/sample_files.py
@@ -7,28 +7,84 @@ import requests
 # ASSUMES THE SUBFOLDERS ARE IN THE
 # CURRENT WORKING DIRECTORY OF THE SCRIPT FILE
 DIR_PATH = os.getcwd()
-POOL_DIR = DIR_PATH + "/pool/"
-SAMPLE_DIR = DIR_PATH + "/samples/"
+POOL_DIR = DIR_PATH + "/Library/"
+CHALLENGE_DIR = DIR_PATH + "/Challenges/"
 # SAMPLE_SIZE = int(input("How many samples? "))
 SAMPLE_SIZE = 10
-FAIL_DIR = DIR_PATH + '/failure/'
-COMPLETE_DIR = DIR_PATH + '/success/'
+FAIL_DIR = DIR_PATH + '/Failures/'
+COMPLETE_DIR = DIR_PATH + '/Complete/'
+REWARD_DIR = DIR_PATH + '/Rewards/'
 
 file_list = [
     os.path.join(path, name) for path, subdirs, files in os.walk(POOL_DIR)
     for name in files
 ]
-sample_list = [
-    os.path.join(path, name) for path, subdirs, files in os.walk(SAMPLE_DIR)
+
+challenge_list = [
+    os.path.join(path, name) for path, subdirs, files in os.walk(CHALLENGE_DIR)
     for name in files
 ]
 
-if len(sample_list) > 0:
-    print("Sample list is not empty, moving all samples back to pool...")
-    for file in sample_list:
-        filename = os.path.basename(file)
-        target = POOL_DIR + filename
-        os.rename(file, target)
+fail_list = [
+    os.path.join(path, name) for path, subdirs, files in os.walk(FAIL_DIR)
+    for name in files
+]
+
+reward_list = [
+    os.path.join(path, name) for path, subdirs, files in os.walk(REWARD_DIR)
+    for name in files
+]
+
+
+def get_filename(file):
+    filename, extension = os.path.splitext(file)
+    filename = os.path.basename(file)
+    return filename
+
+
+def move_to_challenges(file):
+    filename, extension = os.path.splitext(file)
+    filename = os.path.basename(file)
+    target = CHALLENGE_DIR + filename
+    os.rename(file, target)
+
+
+def move_to_complete(file):
+    filename, extension = os.path.splitext(file)
+    filename = os.path.basename(file)
+    target = COMPLETE_DIR + filename
+    os.rename(file, target)
+
+
+def move_to_fail(file):
+    filename, extension = os.path.splitext(file)
+    filename = os.path.basename(file)
+    target = FAIL_DIR + filename
+    os.rename(file, target)
+
+
+def move_to_rewards(file):
+    filename, extension = os.path.splitext(file)
+    filename = os.path.basename(file)
+    target = REWARD_DIR + filename
+    os.rename(file, target)
+
+
+def move_to_library(file):
+    filename, extension = os.path.splitext(file)
+    filename = os.path.basename(file)
+    target = POOL_DIR + filename
+    os.rename(file, target)
+
+
+if len(challenge_list) > 0:
+    print("Challenge list is not empty, moving all files back to pool...")
+    for file in challenge_list:
+        move_to_library(file)
+if len(reward_list) > 0:
+    print("Reward list is not empty, moving all files back to pool...")
+    for file in reward_list:
+        move_to_library(file)
 
 print("Drawing {} new files for sample pool...".format(SAMPLE_SIZE))
 sample_list = random.sample(file_list, SAMPLE_SIZE)
@@ -36,61 +92,61 @@ sample_list = random.sample(file_list, SAMPLE_SIZE)
 for i, file in enumerate(sample_list):
     filename, extension = os.path.splitext(file)
     filename = os.path.basename(file)
-    print("File #{} is {}".format(i, filename))
-    # target = SAMPLE_DIR + filename
-    # os.rename(file, target)
+    print("#{} is {}".format(i, filename))
 
-ans = input("See anything you like? (int / N)")
+ans = input("Pick a number: ")
 
-if not ans.lower.startswith("n"):
-    pass
+target = sample_list[int(ans)]
+print("You have chosen {} as your reward.".format(get_filename(target)))
+move_to_rewards(target)
 
-print("Picking conditional test...")
-cond = random.choice(file_list)
+print("To be rewarded, you have to pass a challenge.")
 
-print("Your conditional test is "")
-print(cond)
+if len(fail_list) > 0:
+    print("Fail list is not empty, picking from fail pool...")
+    cond = random.choice(fail_list)
+else:
+    print("Picking conditional test from the library...")
+    cond = random.choice(file_list)
 
-sample_list = [
-    os.path.join(path, name) for path, subdirs, files in os.walk(SAMPLE_DIR)
+print("Your challenge is: {}".format(get_filename(cond)))
+
+move_to_challenges(cond)
+
+challenge_list = [
+    os.path.join(path, name) for path, subdirs, files in os.walk(CHALLENGE_DIR)
     for name in files
 ]
 
+reward_list = [
+    os.path.join(path, name) for path, subdirs, files in os.walk(REWARD_DIR)
+    for name in files
+]
 
-def scrape_response(tag):
-    # url =
-    pass
+print("Procceed with your challenge.")
+input("Press Enter to continue...")
 
 
-for file in sample_list:
-    target = None
-    filename = os.path.basename(file)
-    picked = input("Did you pick file {}? (Y/N)".format(filename))
-    if picked.lower().startswith("y"):
-        success = input("Were you successful? (Y/N)")
-        if success.lower().startswith("y"):
-            print("********** SUCCESS!!! ***********")
-            print("Moving the file to the success folder....")
-            r = requests.get('http://api.quotable.io/random')
-            print(r.json()["content"])
-            giphy_url = giphy_url.format("win")
-            target = COMPLETE_DIR + filename
-        else:
-            print("********** WEAK!!! ***********")
-            print("Adding the file to the list of your failures...")
-            r = requests.get('https://api.adviceslip.com/advice')
-            print(r.json()["slip"]["advice"])
-            giphy_url = giphy_url.format("fail")
-            target = FAIL_DIR + filename
-    else:
-        print("**********  SCARED???  ***********")
-        print("Moving {} back to pool..".format(filename))
-        target = POOL_DIR + filename
+def success(file):
+    f = get_filename(file)
+    stat = input("Did you succeed with {}? (Y/N)".format(f))
+    if stat.lower().startswith("y"):
+        print("********** SUCCESS!!! ***********")
+        print("Moving {} to the success folder....".format(f))
         r = requests.get('http://api.quotable.io/random')
         print(r.json()["content"])
-        giphy_url = giphy_url.format("pussy")
-    if target:
-        os.rename(file, target)
-        response = requests.request(
-            "GET", url).json()["data"]["images"]["original"]["url"]
-        print(response)
+        move_to_complete(file)
+        return True
+    else:
+        print("********** WEAK!!! ***********")
+        print("Adding {} to the list of your failures...".format(f))
+        r = requests.get('https://api.adviceslip.com/advice')
+        print(r.json()["slip"]["advice"])
+        move_to_fail(file)
+    return False
+
+
+for file in challenge_list:
+    if success(file):
+        for f in reward_list:
+            success(f)
